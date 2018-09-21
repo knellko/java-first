5273fb31eb47436ad1aa6c55ed82c28469c0a67e
knellko <43444853+knellko@users.noreply.github.com>
2018-09-21 15:14:30 +0300

    Style settings changed

--- sandbox/src/main/java/ru/qa/sandbox/prog.java
+++ sandbox/src/main/java/ru/qa/sandbox/prog.java
@@ -2,7 +2,7 @@ package ru.qa.sandbox;
 
 public class prog {
 
-public static void main (String [] args){
-System.out.println ("Hello world");
-}
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
 }
